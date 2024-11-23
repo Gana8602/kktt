bd87296f738baa0f4f493fba017ef7649e75cbc8
Gana8602 <ganapathi8602@gmail.com>
2024-11-23 12:38:14 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:task_management_app/screens/home.dart';
+import 'package:task_management_app/screens/profile.dart';
 import 'package:task_management_app/screens/welcome_screens.dart';
 import 'screens/add_project_screens.dart';
 import 'screens/dashboard_screens.dart';
@@ -25,7 +28,7 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.poppinsTextTheme(),
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const WelcomeScreen(),
     );
   }
 }
@@ -40,49 +43,44 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
   final List<Widget> _screens = [
-    const WelcomeScreen(),
     const DashboardScreen(),
     const TaskListScreen(),
     AddProjectScreen(),
+    const HomePage(),
+    Profile()
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
+
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: CurvedNavigationBar(
+        index: _currentIndex,
+        color: Color(0xFF90d5ff).withOpacity(0.3),
+        backgroundColor: Colors.white,
+        buttonBackgroundColor: Colors.blue,
+        items: <Widget>[
+          Icon(Icons.home, size: 20),
+          Icon(Icons.calendar_month, size: 20),
+          Icon(
+            Icons.add,
+            size: 20,
+          ),
+          Icon(Icons.edit_document, size: 20),
+          Icon(
+            Icons.people_alt,
+            size: 20,
+          )
+        ],
         onTap: (index) {
           setState(() {
             _currentIndex = index;
           });
+
+          //Handle button tap
         },
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                color: Colors.blue,
-              ),
-              label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.dashboard,
-                color: Colors.blue,
-              ),
-              label: 'Dashboard'),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.task,
-                color: Colors.blue,
-              ),
-              label: 'Tasks'),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.add,
-                color: Colors.blue,
-              ),
-              label: 'Add Project'),
-        ],
       ),
     );
   }
--- lib/screens/welcome_screens.dart
+++ lib/screens/welcome_screens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_management_app/main.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -7,29 +8,69 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          
-          children: [
-            
-            const Text(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 1.4,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/welcome.png"),
+                    fit: BoxFit.cover)),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 1.4,
+            child: const Text(
               "Task Management & To-Do List",
+              textAlign: TextAlign.center,
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 20),
-            Container(
-              color: Colors.blue,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Go to next page
-                },
-                child: const Text("Let's Start"),
-              
-              ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 1.5,
+            child: Text(
+              "This Productive tool is designed to help you better manage your task project-wise conveniently!",
+              textAlign: TextAlign.center,
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 20),
+          InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()));
+            },
+            child: Container(
+                height: 50,
+                width: MediaQuery.of(context).size.width / 1.2,
+                decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Spacer(),
+                    Text(
+                      "Let's Start",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Icon(
+                        Icons.arrow_right_rounded,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                )),
+          ),
+        ],
       ),
     );
   }
